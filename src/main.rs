8b5f123efe862e7c46412f9f4bb259dcc90e8dc5
Mitchell Hentges <mitch9654@gmail.com>
2017-01-21 18:57:38 -0800

    Split header reading, read version

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
+use std::str;
 
 const GCNO_MAGIC: u32 = 0x67636e6f;
 
@@ -18,16 +19,32 @@ fn main() {
         let mut file = match File::open(&path) {
             Err(e) => {
                 writeln!(std::io::stderr(), "Failed to open {}:{}", &gcno_path, e.description()).unwrap();
-                return;
+                std::process::exit(1);
             }
             Ok(file) => file
         };
 
         let mut buffer = Vec::<u8>::new();
         file.read_to_end(&mut buffer).unwrap();
-        let magic = LittleEndian::read_u32(&buffer[0..4]);
-        println!("{:X}, {:X}, {}", magic, GCNO_MAGIC, magic == GCNO_MAGIC)
+        if let Err(code) = parse_header(&buffer) {
+            std::process::exit(code);
+        }
+
+        let offset = 12u8; // Start after magic, version, stamp
+
     } else {
         println!("Usage: bohemian-waxwing PATH_TO_GCNO");
     }
+}
+
+fn parse_header(buffer: &Vec<u8>) -> Result<(), i32> {
+    if GCNO_MAGIC != LittleEndian::read_u32(&buffer[0..4]) {
+        return Err(2);
+    };
+
+    if let Ok(ref version) = str::from_utf8(&buffer[4..8]) {
+        println!("version: {}", version);
+    };
+
+    return Ok(());
 }
